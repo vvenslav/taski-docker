4d1a9f9d6fc81769b69cceac1502c7f74317ef77
Влад Венслав <Venslav23@yandex.ru>
2025-09-22 18:34:16 +0300

    Add workflow with PEP8 linting 2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,9 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '89.169.167.119', '127.0.0.1', 'venslav.zapto.org']
+ALLOWED_HOSTS = [
+    'localhost', '89.169.167.119', '127.0.0.1', 'venslav.zapto.org'
+]
 
 
 # Application definition
