75f68d0959b022a5284bfea086fa07c64f71c1fd
Влад Венслав <Venslav23@yandex.ru>
2025-10-01 15:03:07 +0300

    action

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost', '89.169.167.119', '127.0.0.1', 'venslav.zapto.org'
+    'localhost', '89.169.167.119', '127.0.0.1', 'venslav.zapto.org', 'backend'
 ]
 
 
